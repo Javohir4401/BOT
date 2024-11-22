03cc604282082b476a15c10dabf51494680adab6
_m1ster_uzbek_ <jakbarov4401@gmail.com>
2024-11-22 18:26:46 +0500

    config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'botToken' => "7678622082:AAF6l8QQ7-iEqq33OwCx52AB2YyxXQtmSjc",
+    'weatherApiToken' => "570d6bcba80a484fabbf080f31f3f185",
+    'aladhanApiUrl' => "https://api.aladhan.com/v1/timingsByAddress",
+    'cbuCurrencyApiUrl' => "https://cbu.uz/uz/arkhiv-kursov-valyut/json/",
+    
+    'dbConfig' => [
+        'host' => 'localhost',
+        'db' => 'telegram_bot',
+        'user' => 'root',
+        'pass' => '1112'
+    ]
+];
